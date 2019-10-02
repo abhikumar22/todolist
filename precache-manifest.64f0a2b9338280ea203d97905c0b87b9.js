f9dbf9f7923eeea0d8b64ca28eb55ea63b116486
Abhishek Kumar <abhi.ckp1002@gmail.com>
2019-10-02 20:23:28 +0530

    Updates

---
+++ precache-manifest.64f0a2b9338280ea203d97905c0b87b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedbcfd908ac82543625a92fe080e3d4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "17e6ed343c8ac1beea25",
+    "url": "/todolist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "900ffbbdf1df05ac6f87",
+    "url": "/todolist/static/js/2.1387e33b.chunk.js"
+  },
+  {
+    "revision": "17e6ed343c8ac1beea25",
+    "url": "/todolist/static/js/main.bb621ff0.chunk.js"
+  },
+  {
+    "revision": "4e1b8ed3c740f7eb5649",
+    "url": "/todolist/static/js/runtime-main.1395c765.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todolist/static/media/logo.25bf045c.svg"
+  }
+]);
